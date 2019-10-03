6f98f41a40e2ff3a3eee31ce67195486e40e86cc
Jesse Krim <jesse.krim@gmail.com>
2019-10-03 11:33:51 -0400

    first commit

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#f0e6f6',
+
+  primary: '#ccc',
+  primaryLight: '#ae66df',
+  primaryDark: '#4b007d',
+
+  secondary: '#1dacd6',
+  secondaryLight: '#ff79b0',
+  secondaryDark: '#c60055',
+};
